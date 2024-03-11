af3af9dd1faa3eb75a43dccbb5bb4ebd23bb2ca0
Mathys <mathystheolade973@gmail.com>
2024-03-11 15:58:11 +0100

    feat: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,27 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  root: true,
   env: {
-    node: true,
+    browser: true,
+    es2021: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'warn',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
-    '@typescript-eslint/require-await': 'warn',
-    'array-callback-return': 'warn',
-    'comma-dangle': [
-      'warn',
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'never',
-        exports: 'never',
-        functions: 'only-multiline',
-      },
-    ],
-    'dot-notation': 'warn',
-    'eol-last': ['warn', 'always'],
-    'max-len': ['warn', { code: 180 }],
-    'no-multiple-empty-lines': ['warn', { max: 2 }],
-    'no-debugger': 'warn',
-    'no-return-await': 'warn',
-    'no-shadow': 'off',
-    'no-trailing-spaces': 'warn',
-    'no-unneeded-ternary': 'warn',
-    'no-unused-expressions': 'warn',
-    quotes: ['warn', 'single'],
-    'require-await': 'off',
-    semi: ['warn', 'always'],
-    'spaced-comment': ['warn', 'always'],
-  },
-};
+    semi: 'on'
+  }
+}
