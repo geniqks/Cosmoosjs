a302be53d1043c72209f267427a1aae46ac8ad9e
Mathys <mathystheolade973@gmail.com>
2024-03-12 23:55:54 +0100

    wip: cosmos factory

--- packages/core/src/cosmos-factory.ts
+++ packages/core/src/cosmos-factory.ts
@@ -1,3 +1,14 @@
-export class CosmosFactory {
+import { injectable } from "inversify";
+import { CosmosOptions } from "./interfaces";
 
+@injectable()
+export class CosmosFactory {
+  public async create(options: CosmosOptions): Promise<any> {
+
+  }
 }
+
+/**
+ * Run the application
+ */
+export const CosmosFactoryStatic = new CosmosFactory();
--- packages/core/src/index.ts
+++
@@ -1,3 +0,0 @@
-export function salutTest () {
-  return 1
-};
