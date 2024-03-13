c67f5019b60df8e76edeb62fa033c451679ef714
Mathys <mathystheolade973@gmail.com>
2024-03-13 23:21:46 +0100

    update location

--- packages/core/index.ts
+++
@@ -1,9 +0,0 @@
-import 'reflect-metadata';
-
-export { CosmosFactory } from './src/cosmos-factory';
-export * from './src/interfaces';
-export * from './src/ioc';
-
-export function salutTest () {
-  return 1
-};
---
+++ packages/core/src/index.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata';
+
+export { CosmosFactory } from "./cosmos-factory";
+export * from "./interfaces";
+export * from './ioc';
+
+export function salutTest () {
+  return 1
+};
