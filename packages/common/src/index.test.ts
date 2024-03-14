048c61333ff41aea6c7715821d43f64f5a01a4aa
Mathys <mathystheolade973@gmail.com>
2024-03-14 23:31:35 +0100

    remove unused test file

--- packages/common/src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { salutTest } from '@cosmosjs/core';
-import { describe, expect, it } from 'bun:test';
-
-describe('test', () => {
-  it('Should remove custom param to *', () => {
-    expect(salutTest()).toEqual(1);
-  });
-});
