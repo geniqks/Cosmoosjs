4c55b680901af1709e7d5b549b4c2d36fd05f303
Mathys <mathystheolade973@gmail.com>
2024-03-14 23:43:21 +0100

    Enhance comments

--- sample/hono-hello-world/src/start/env.ts
+++ sample/hono-hello-world/src/start/env.ts
@@ -1,5 +1,15 @@
 import { Env } from "@cosmosjs/core";
 
+/**
+ * This file is used to validate environment variables
+ * We use zod to validate them
+ * 
+ * You can access zod directly with the const
+ * Env.validator
+ * 
+ * For more information on the zod api
+ * @link https://zod.dev/
+ */ 
 export default {
   PORT: Env.validator.string().transform(Number)
 }
--- sample/hono-hello-world/src/start/ioc-loader.ts
+++ sample/hono-hello-world/src/start/ioc-loader.ts
@@ -4,7 +4,8 @@ import type { Container } from "inversify"
 /**
  * This file will list all the application's injectables. 
  * They will then be injected into our ioc library "Inversify".
- * For more information @link https://inversify.io/
+ * For more information 
+ * @link https://inversify.io/
  */
 export default (container: Container) => {
   container.bind(Validator).toSelf()
