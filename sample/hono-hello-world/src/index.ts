06eb154913481d11d7a986eb2fd2caeb980e11a5
Mathys <mathystheolade973@gmail.com>
2024-03-18 00:02:28 +0100

    add new app boostrap

--- sample/hono-hello-world/src/index.ts
+++ sample/hono-hello-world/src/index.ts
@@ -1,35 +1,41 @@
-import { defineConfigAndBootstrapApp } from "@cosmosjs/core";
-import {serve} from 'bun';
-import dotenv from 'dotenv'
-dotenv.config()
+import { IocContainer, type LoggerService, defineConfigAndBootstrapApp } from '@cosmosjs/core';
+import { serve } from 'bun';
+import dotenv from 'dotenv';
+dotenv.config();
 
 /**
  * The server will be configured with all given options
  * and will return the http config in order to start the server.
  */
 const boostrapApp = async () => {
-  const config = await defineConfigAndBootstrapApp({
-    adapters: {
-      server: {
-        port: 3005,
-        provider: () => import('@cosmosjs/hono-openapi')
-      },
-    },
-    loaders: {
-      env: () => import('@start/env'),
-      ioc: () => import('@start/ioc-loader'),
-    },
-    entrypoint: () => import('@app/index'),
-  })
-  return config;
-}
+	const config = await defineConfigAndBootstrapApp({
+		adapters: {
+			server: {
+				port: 3005,
+				provider: () => import('@cosmosjs/hono-openapi'),
+			},
+		},
+		loaders: {
+			env: () => import('@start/env'),
+			ioc: () => import('@start/ioc-loader'),
+		},
+		entrypoint: () => import('@app/index'),
+	});
+	return config;
+};
 
-boostrapApp().then(httpConfig => {
-  serve({
-    async fetch(req) {
-      const serverConfig = await httpConfig?.fetch(req);
-      return serverConfig;
-    },
-    port: httpConfig?.port
-  })
-})
+boostrapApp().then((httpConfig) => {
+  const logger = IocContainer.container.get<LoggerService>('LoggerService');
+  try{
+    serve({
+      async fetch(req) {
+        const serverConfig = await httpConfig?.fetch(req);
+        return serverConfig;
+      },
+      port: httpConfig?.port,
+    });
+    logger.pino.info(`Hono 🥟 Server Listening on port ${httpConfig?.port}`);
+  }catch(e) {
+    logger.pino.error(`An error occurred during server initialization, ${e}`);
+  }
+});
