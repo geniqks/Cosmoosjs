c2f869ab5db60cc14431e418c97f749aae212a42
Mathys <mathystheolade973@gmail.com>
2024-03-17 17:20:12 +0100

    wip: add hono to sample

--- sample/hono-hello-world/src/index.ts
+++ sample/hono-hello-world/src/index.ts
@@ -1,13 +1,35 @@
 import { defineConfigAndBootstrapApp } from "@cosmosjs/core";
+import {serve} from 'bun';
 import dotenv from 'dotenv'
 dotenv.config()
 
-defineConfigAndBootstrapApp({
-  adapters: {
-    server: () => import('@cosmosjs/hono-openapi'),
-  },
-  loaders: {
-    env: () => import('@start/env'),
-    ioc: () => import('@start/ioc-loader'),
-  }
+/**
+ * The server will be configured with all given options
+ * and will return the http config in order to start the server.
+ */
+const boostrapApp = async () => {
+  const config = await defineConfigAndBootstrapApp({
+    adapters: {
+      server: {
+        port: 3005,
+        provider: () => import('@cosmosjs/hono-openapi')
+      },
+    },
+    loaders: {
+      env: () => import('@start/env'),
+      ioc: () => import('@start/ioc-loader'),
+    },
+    entrypoint: () => import('@app/index'),
+  })
+  return config;
+}
+
+boostrapApp().then(httpConfig => {
+  serve({
+    async fetch(req) {
+      const serverConfig = await httpConfig?.fetch(req);
+      return serverConfig;
+    },
+    port: httpConfig?.port
+  })
 })
