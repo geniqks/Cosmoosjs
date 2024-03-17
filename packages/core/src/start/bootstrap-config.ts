55df9c28ff4c03869a6dc24b18306c24261040f2
Mathys <mathystheolade973@gmail.com>
2024-03-17 17:20:56 +0100

    wip: bootstrap config

--- packages/core/src/start/bootstrap-config.ts
+++ packages/core/src/start/bootstrap-config.ts
@@ -9,11 +9,25 @@ async function loadModule(importedModule: any) {
 		throw new Error(`Error loading module ${importedModule}: ${error?.message}`);
 	}
 }
-
-export async function defineConfigAndBootstrapApp(config: IBootstrapConfig) {
+// TODO: refacto this file later
+export async function defineConfigAndBootstrapApp(config: IBootstrapConfig): Promise<{
+	port: number;
+	fetch: any;
+} | void> {
 	const envLoader = await loadModule(config.loaders.env);
 	const iocBindingLoader = await loadModule(config.loaders.ioc);
 	const env = IocContainer.container.get(Env);
 	env.process(envLoader);
 	iocBindingLoader(IocContainer.container);
+
+	if (config.entrypoint) {
+		const entrypoint = await loadModule(config.entrypoint);
+		entrypoint();
+	}
+
+	if (config.adapters?.server) {
+		const server = await config.adapters?.server.provider();
+		console.log(server);
+		return server.HonoFactory.listen(config.adapters?.server.port, IocContainer.container);
+	}
 }
