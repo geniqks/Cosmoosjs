ab1a84c5ffeb61a840708a9fc6dd5b7df24d4401
Mathys <mathystheolade973@gmail.com>
2024-03-25 12:21:17 +0100

    remove inversify-inject-decorators

--- packages/core/src/utils/lazy-inject.util.ts
+++
@@ -1,6 +0,0 @@
-import type { Container } from 'inversify';
-import getDecorators from 'inversify-inject-decorators';
-import { CONTAINER, SERVER_TARGET } from 'src/constants/reflector.constant';
-
-const container: Container = Reflect.getMetadata(CONTAINER, SERVER_TARGET);
-export const { lazyInject } = getDecorators(container);
