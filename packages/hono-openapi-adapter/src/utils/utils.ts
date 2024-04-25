6bd0da80e83265f38a50d7f9f74c2dd6c522a389
Mathys <mathystheolade973@gmail.com>
2024-04-25 21:26:29 +0200

    remove utils

--- packages/hono-openapi-adapter/src/utils/utils.ts
+++
@@ -1,7 +0,0 @@
-export function serializeRoutePath(routePath: string): string {
-  // To find all string with pattern {...} to after replace them by *
-  const regex = /\{[^}]+\}/g;
-  const serializedPath = routePath.replace(regex, '*');
-
-  return serializedPath;
-}
