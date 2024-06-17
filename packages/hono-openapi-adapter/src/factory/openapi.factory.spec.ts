5ed16771b28e58e3c6fc9cd38a750a89a1550926
Mathys <mathystheolade973@gmail.com>
2024-06-17 21:59:56 +0200

    add get test for openapiFactory

--- packages/hono-openapi-adapter/src/factory/openapi.factory.spec.ts
+++ packages/hono-openapi-adapter/src/factory/openapi.factory.spec.ts
@@ -4,13 +4,19 @@ import { OpenapiFactory } from './openapi.factory';
 
 describe.only('OpenApiFactory', () => {
   const server = new OpenAPIHono();
-  function getRequestFormatted(method: string, body: Object): RequestInit {
+  function requestFormatterHelper(method: 'post' | 'get', body?: Object): RequestInit {
+    if (method === 'post') {
+      return {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      };
+    }
+
     return {
       method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
     };
   }
 
@@ -93,7 +99,7 @@ describe.only('OpenApiFactory', () => {
     it.only('it let the request go', async () => {
       const res = await server.request(
         '/schemaValidation',
-        getRequestFormatted('post', {
+        requestFormatterHelper('post', {
           email: 'email@gmail.com',
           name: 'test',
           password: '123AZN',
@@ -110,7 +116,7 @@ describe.only('OpenApiFactory', () => {
     it.only('it return an error because name is missing  ', async () => {
       const res = await server.request(
         '/schemaValidation',
-        getRequestFormatted('post', {
+        requestFormatterHelper('post', {
           email: 'email@gmail.com',
           password: '123AZN',
         }),
@@ -126,7 +132,7 @@ describe.only('OpenApiFactory', () => {
     it.only('it should return an error because email is not formatted correctly', async () => {
       const res = await server.request(
         '/schemaValidation',
-        getRequestFormatted('post', {
+        requestFormatterHelper('post', {
           email: 'email',
           name: 'test',
           password: '123AZN',
@@ -143,7 +149,7 @@ describe.only('OpenApiFactory', () => {
     it.only('it should return an error because name.length < 3', async () => {
       const res = await server.request(
         '/schemaValidation',
-        getRequestFormatted('post', {
+        requestFormatterHelper('post', {
           email: 'email@gmail.cm',
           name: '12',
           password: '123AZN',
@@ -160,12 +166,93 @@ describe.only('OpenApiFactory', () => {
     it.only('it should return an error because name.length > 6', async () => {
       const res = await server.request(
         '/schemaValidation',
-        getRequestFormatted('post', {
+        requestFormatterHelper('post', {
           email: 'email@gmail.cm',
           name: '1234567',
           password: '123AZN',
         }),
       );
+
+      const response = await res.json();
+      expect(res.status).toEqual(400);
+      expect(response).toMatchObject({
+        success: false,
+      });
+    });
+  });
+
+  describe.only('get requests', async () => {
+    const testGetSchema = OpenapiFactory.generateSchema({
+      schemaName: 'testSchema',
+      params: [
+        {
+          required: true,
+          type: 'string',
+          name: 'email',
+          rules: [
+            {
+              functionName: 'email',
+            },
+          ],
+        },
+      ],
+    });
+
+    const getRoute = createRoute({
+      method: 'get',
+      path: '/schemaGetValidation',
+      request: {
+        query: testGetSchema,
+      },
+      responses: {
+        200: {
+          content: {
+            'application/json': {
+              schema: z.object({
+                message: z.string(),
+              }),
+            },
+          },
+          description: 'ok',
+        },
+        400: {
+          content: {
+            'application/json': {
+              schema: z.object({}),
+            },
+          },
+          description: 'zod error',
+        },
+      },
+    });
+    server.openapi(getRoute, async (ctx) => {
+      return ctx.json({
+        message: 'ok',
+      });
+    });
+
+    it.only('it should return an error because email is missing', async () => {
+      const res = await server.request('/schemaGetValidation', requestFormatterHelper('get'));
+
+      const response = await res.json();
+      expect(res.status).toEqual(400);
+      expect(response).toMatchObject({
+        success: false,
+      });
+    });
+
+    it.only('it should let the request go', async () => {
+      const res = await server.request('/schemaGetValidation?email=hello@gmail.com', requestFormatterHelper('get'));
+
+      const response = await res.json();
+      expect(res.status).toEqual(200);
+      expect(response).toEqual({
+        message: 'ok',
+      });
+    });
+
+    it.only('it should return an error because email is not formatted correctly', async () => {
+      const res = await server.request('/schemaGetValidation?email=hello', requestFormatterHelper('get'));
 
       const response = await res.json();
       expect(res.status).toEqual(400);
