e9282c852bfba47e26426ca8cf28a75e232b000f
Mathys <mathystheolade973@gmail.com>
2024-06-17 23:16:00 +0200

    add tests for hono adapter

--- packages/hono-openapi-adapter/src/start/hono-adapter.spec.ts
+++ packages/hono-openapi-adapter/src/start/hono-adapter.spec.ts
@@ -1,22 +1,54 @@
-import { describe, expect, it } from 'bun:test';
+import { beforeAll, describe, expect, it } from 'bun:test';
 import { type IHttpServe, IocContainer } from '@cosmoosjs/core';
 import type { FactoryConfig } from '@customTypes/index';
+import { OpenAPIHono, createRoute } from '@hono/zod-openapi';
 import { Server } from '@server/server';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import z from 'zod';
 import { HttpFactory } from './hono-adapter';
 
 describe.only('HonoAdapter', () => {
   const adapter = HttpFactory;
   const container = IocContainer.container;
   adapter.bindContainers(container);
+  const server = container.get(Server);
+
+  beforeAll(() => {
+    // Setup routes
+    function setErrorRoute(path: string) {
+      const route = createRoute({
+        method: 'get',
+        path,
+        responses: {
+          200: {
+            content: {
+              'application/json': {
+                schema: z.object({
+                  message: z.string(),
+                }),
+              },
+            },
+            description: 'ok',
+          },
+        },
+      });
+      server.hono.openapi(route, async (_ctx) => {
+        throw new Error();
+      });
+    }
+
+    setErrorRoute('/error');
+    setErrorRoute('/');
+  });
 
   it.only('it should get the server', async () => {
-    const server = container.get(Server);
     expect(server).toBeTruthy();
     expect(server).toBeInstanceOf(Server);
+    expect(server.hono).toBeInstanceOf(OpenAPIHono);
   });
 
   it.only('it should config and return an object with type IHttpServe', async () => {
-    const serverConfig: FactoryConfig<string> = {
+    const serverConfig: FactoryConfig = {
       port: 3001,
       metadata: {
         enableSwaggerInProd: false,
@@ -47,5 +79,33 @@ describe.only('HonoAdapter', () => {
     const httpServerConfig = adapter.listen(serverConfig);
     expect(httpServerConfig).toMatchObject(HttpServer);
     expect(httpServerConfig.port).toEqual(3001);
+  });
+
+  it.only('it should return an error', async () => {
+    const res = await server.hono.request('/', {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/text',
+      },
+    });
+    const response = await res.text();
+    expect(res.status).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response).toEqual(ReasonPhrases.INTERNAL_SERVER_ERROR);
+  });
+
+  it.only('it should setup a custom error handler', async () => {
+    adapter.exceptionHandler((_err, ctx, _logger) => {
+      return ctx.text('custom error', StatusCodes.INTERNAL_SERVER_ERROR);
+    });
+
+    const res = await server.hono.request('/error', {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/text',
+      },
+    });
+    const response = await res.text();
+    expect(res.status).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response).toEqual('custom error');
   });
 });
